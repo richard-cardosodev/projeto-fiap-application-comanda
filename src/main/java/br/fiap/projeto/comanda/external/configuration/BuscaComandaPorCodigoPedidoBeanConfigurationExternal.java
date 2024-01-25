7e78272b078ebe985ee38db6334e928b829c0f74
Richard Cardoso <richard.166@hotmail.com>
2024-01-25 18:05:50 -0300

    fix: build

--- src/main/java/br/fiap/projeto/comanda/external/configuration/BuscaComandaPorCodigoPedidoBeanConfigurationExternal.java
+++ src/main/java/br/fiap/projeto/comanda/external/configuration/BuscaComandaPorCodigoPedidoBeanConfigurationExternal.java
@@ -1,24 +1,14 @@
 package br.fiap.projeto.comanda.external.configuration;
 
+import br.fiap.projeto.comanda.adapter.gateway.BuscaPorComandaPorCodigoPedidoGatewayAdapter;
+import br.fiap.projeto.comanda.external.repository.postgres.SpringComandaRepository;
+import br.fiap.projeto.comanda.usecase.exception.EntradaInvalidaException;
+import br.fiap.projeto.comanda.usecase.port.repository.IBuscarPorComandaPorCodigoPedidoRepositoryUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import br.fiap.projeto.comanda.adapter.gateway.BuscaPorComandaPorCodigoPedidoGatewayAdapter;
-import br.fiap.projeto.comanda.external.repository.postgres.SpringComandaRepository;
-import br.fiap.projeto.comanda.usecase.BuscaComandaPorCodigoPedidoUseCase;
-import br.fiap.projeto.comanda.usecase.exception.EntradaInvalidaException;
-import br.fiap.projeto.comanda.usecase.port.interfaces.IBuscaPorComandaPorCodigoPedidoUseCase;
-import br.fiap.projeto.comanda.usecase.port.repository.IBuscarPorComandaPorCodigoPedidoRepositoryUseCase;
-
 @Configuration
 public class BuscaComandaPorCodigoPedidoBeanConfigurationExternal {
-
-    @Bean
-    IBuscaPorComandaPorCodigoPedidoUseCase buscaComandaPorCodigoPedidoUseCase(
-            IBuscarPorComandaPorCodigoPedidoRepositoryUseCase buscarComandaPorCodigoPedidoRepositoryUseCase)
-            throws EntradaInvalidaException, Exception {
-        return new BuscaComandaPorCodigoPedidoUseCase(buscarComandaPorCodigoPedidoRepositoryUseCase);
-    }
 
     @Bean
     IBuscarPorComandaPorCodigoPedidoRepositoryUseCase buscarComandaPorCodigoPedidoRepositoryUseCase(
